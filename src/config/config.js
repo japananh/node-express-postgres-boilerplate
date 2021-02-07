ca4cfc1c305847a937217e263be0376d60deeb99
Tran Nhat Anh <japananh@gmail.com>
2021-02-07 17:30:30 +0700

    add config file

---
+++ src/config/config.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const Joi = require('@hapi/joi');
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVarsSchema = Joi.object()
+	.keys({
+		NODE_ENV: Joi.string()
+			.valid('production', 'development', 'test')
+			.required(),
+		PORT: Joi.number().default(3000),
+
+		JWT_SECRET: Joi.string().required().description('JWT secret key'),
+		JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+			.default(30)
+			.description('minutes after which access tokens expire'),
+		JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+			.default(30)
+			.description('days after which refresh tokens expire'),
+		SQL_USER: Joi.string().description('sqldb username'),
+		SQL_HOST: Joi.string().description('sqldb host'),
+		SQL_DATABASE_NAME: Joi.string().description('sqldb database name'),
+		SQL_PASSWORD: Joi.string().description('sqldb password'),
+	})
+	.unknown();
+
+const { value: envVars, error } = envVarsSchema
+	.prefs({ errors: { label: 'key' } })
+	.validate(process.env);
+
+if (error) {
+	throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = {
+	env: envVars.NODE_ENV,
+	port: envVars.PORT,
+	jwt: {
+		secret: envVars.JWT_SECRET,
+		accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+		refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+		resetPasswordExpirationMinutes: 10,
+	},
+	sqlDB: {
+		user: envVars.SQL_USER,
+		host: envVars.SQL_HOST,
+		database: envVars.SQL_DATABASE_NAME,
+		password: envVars.SQL_PASSWORD,
+	},
+};
---
+++ src/config/jwt.js
@@ -0,0 +1,27 @@
+const expressJwt = require('express-jwt');
+const config = require('./config.js');
+
+async function isRevoked(_req, _payload, done) {
+	done();
+}
+
+function jwt() {
+	const { secret } = config.jwt;
+	return expressJwt({
+		secret,
+		getToken: function fromHeaderOrQuerystring(req) {
+			const { token } = req.cookies || {};
+			if (token) return token;
+			return null;
+		},
+		algorithms: ['HS256'],
+		isRevoked,
+	}).unless({
+		path: [
+			// public routes that don't require authentication
+			/\/v[1-9](\d)*\/auth\/.*/,
+		],
+	});
+}
+
+module.exports = jwt;
