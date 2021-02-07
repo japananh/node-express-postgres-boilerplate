b5290aa1e6c2dbdcf33e520a7206c8942215fcb2
Tran Nhat Anh <japananh@gmail.com>
2021-02-07 17:29:22 +0700

    init flyway v1 file

---
+++ flyway/sql/V1__baseline.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS public."user" (
+    "id" SERIAL NOT NULL,
+    "name" varchar (25) NOT NULL,
+    "email" varchar (50) UNIQUE NOT NULL,
+    "role" varchar NOT NULL,
+    "created_date_time" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modified_date_time" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "password" varchar NOT NULL,
+    PRIMARY KEY ("id")
+);
